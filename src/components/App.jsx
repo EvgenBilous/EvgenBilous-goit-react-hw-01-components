b427c18d636a86035c41640aa815e6b1b51416e6
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-07 00:31:51 -0600

    added components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import Profile from './userProfile';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import Statistics from './statistics';
+import FriendList from './FriendList';
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
+      <Statistics />
+      <FriendList />
     </div>
   );
 };
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function FriendList({ friends }) {
+  return (
+    <ul class="friend-list">
+      <div>"test"</div>
+    </ul>
+  );
+}
+export default FriendList;
