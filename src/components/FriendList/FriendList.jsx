8540b345fd83d69f6a84f8b7a81f79e822187baa
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-17 23:11:55 -0600

    fixed folder

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FriendListItem from './FriendListItem';
+import FriendListItem from './FriendlistItem/FriendListItem';
 function FriendList({ friends }) {
   return (
     <ul className="friend-list">
